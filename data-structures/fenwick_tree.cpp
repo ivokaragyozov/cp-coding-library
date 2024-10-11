006833ab6e48565a0b9af4c489930f2874d797cd
Ivo <ivokaragyozov2001@gmail.com>
2024-10-11 21:48:53 +0300

    update fenwick_tree.cpp

--- data-structures/fenwick_tree.cpp
+++ data-structures/fenwick_tree.cpp
@@ -1,31 +1,24 @@
-const int32_t MAX_N = 1e5;
+template <typename T> struct FenwickTree {
+    int treeSize;
+    vector<T> data;
 
-template< typename T >
-struct FenwickTree {
-	int32_t treeSize;
-	T data[MAX_N + 5];	
+    FenwickTree(int _size) : treeSize(_size), data(_size + 1, 0) {}
 
-	void init(int32_t _treeSize) {
-		treeSize = _treeSize;
-	}
+    void update(int ind, T val) {
+        while (ind <= treeSize) {
+            data[ind] += val;
+            ind += (ind & (-ind));
+        }
+    }
 
-	void update(int32_t ind, T val) {
-		while(ind <= treeSize) {
-			data[ind] += val;
-			ind += (ind & (-ind));
-		}
-	}
+    T get(int ind) {
+        T ans{};
+        while (ind > 0) {
+            ans += data[ind];
+            ind -= (ind & (-ind));
+        }
+        return ans;
+    }
 
-	T get(int32_t ind) {
-		T ans{};
-		while(ind > 0) {
-			ans += data[ind];
-			ind -= (ind & (-ind));
-		}
-		return ans;
-	}
-
-	T getInterval(int32_t low, int32_t high) {
-		return get(high) - get(low - 1);
-	}
+    T getInterval(int low, int high) { return get(high) - get(low - 1); }
 };
