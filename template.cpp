0215c4e6ac3531d1e241e8fa2a8be7dd99ceb47c
Ivo Karagyozov <ivokaragyozov@Ivos-MacBook-Pro.local>
2024-10-11 20:34:36 +0300

    rename default_code to template

---
+++ template.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+
+#define debug(x) cout << #x << " " << (x) << endl;
+#define pb push_back
+#define remax(a, b) a = max((a), (b));
+#define remin(a, b) a = min((a), (b));
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+}
