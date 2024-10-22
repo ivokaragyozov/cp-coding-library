8c7db8a038a98a7af8094d9f0d460b271551cd27
Ivo <ivokaragyozov2001@gmail.com>
2024-10-22 15:53:35 +0200

    fix scc.hpp

--- graphs/scc.hpp
+++ graphs/scc.hpp
@@ -1,61 +1,72 @@
+/* -------------------------- SCC -------------------------- */
+#include <set>
+#include <stack>
+#include <vector>
+
+using namespace std;
+
 class SCC {
-private:
-	struct Vertex {
-		bool vis;
-		int32_t comp;
-		std::vector<int32_t> oute, ine;
+  private:
+    struct vertex_t {
+        bool vis;
+        int comp;
+        vector<int> oute, ine;
 
-		Vertex(): vis(false), comp(-1) {}
-	};
+        vertex_t() : vis(false), comp(-1) {}
+    };
 
-	void dfs1(int32_t v, std::stack<int32_t> &st) {
-		g[v].vis = true;
+    void dfs(int v, stack<int> &st) {
+        g[v].vis = true;
 
-		for(auto u : g[v].oute) {
-			if(!g[u].vis) dfs1(u, st);
-		}
+        for (auto u : g[v].oute) {
+            if (!g[u].vis)
+                dfs(u, st);
+        }
 
-		st.push(v);
-	}
+        st.push(v);
+    }
 
-	void dfs2(int32_t v, int32_t comp) {
-		g[v].comp = comp;
+    void dfs_rev(int v, int comp) {
+        g[v].comp = comp;
 
-		for(auto u : g[v].ine) {
-			if(g[u].comp == -1) dfs2(u, comp);
-		}
-	}
+        for (auto u : g[v].ine) {
+            if (g[u].comp == -1)
+                dfs_rev(u, comp);
+        }
+    }
 
-public:
-	std::vector<Vertex> g;
-	std::vector<std::set<int32_t>> dag;
+  public:
+    vector<vertex_t> g;
+    vector<set<int>> dag;
 
-	SCC(int32_t n, std::vector<std::pair<int32_t, int32_t>> edges): g(n) {
-		for(auto e : edges) {
-			g[e.first].oute.pb(e.second);
-			g[e.second].ine.pb(e.first);
-		}
+    SCC(int n, vector<pair<int, int>> edges) : g(n) {
+        for (auto e : edges) {
+            g[e.first].oute.push_back(e.second);
+            g[e.second].ine.push_back(e.first);
+        }
 
-		std::stack<int32_t> st;
-		for(int32_t i = 0; i < n; i++) {
-			if(!g[i].vis) {
-				dfs1(i, st);
-			}
-		}
+        stack<int> st;
+        for (int i = 0; i < n; i++) {
+            if (!g[i].vis) {
+                dfs(i, st);
+            }
+        }
 
-		int32_t comp = 0;
-		while(!st.empty()) {
-			auto v = st.top();
-			st.pop();
+        int comp = 0;
+        while (!st.empty()) {
+            auto v = st.top();
+            st.pop();
 
-			if(g[v].comp == -1) dfs2(v, comp++);
-		}
-		
-		dag.resize(comp);
-		for(auto e : edges) {
-			if(g[e.first].comp != g[e.second].comp) {
-				dag[g[e.first].comp].insert(g[e.second].comp);
-			}
-		}
-	}
+            if (g[v].comp == -1)
+                dfs_rev(v, comp++);
+        }
+
+        dag.resize(comp);
+        for (auto e : edges) {
+            if (g[e.first].comp != g[e.second].comp) {
+                dag[g[e.first].comp].insert(g[e.second].comp);
+            }
+        }
+    }
 };
+/* -------------------------- SCC -------------------------- */
